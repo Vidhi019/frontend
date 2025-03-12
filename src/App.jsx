d5413f9b017c06bba6f86bed8210525145cb24a2
Vidhi019 <vidhisheth742@gmail.com>
2025-03-12 15:06:16 +0530

    soft delete

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Signup from "./pages/signup";
+import SignInPage from "./pages/Signin";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme/theme";
+import DashboardPage from "./pages/DashboardPage";
+import OrganizationPage from "./pages/OrganizationPage";
+import OrganizationForm from "./components/OrganizationForm";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/signIn" element={<SignInPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/organizationPage" element={<OrganizationPage />} />
+        <Route path="/organizationForm" element={<OrganizationForm />} />
+      </Routes>
+    </ThemeProvider>
+  );
+}
+
+export default App;
